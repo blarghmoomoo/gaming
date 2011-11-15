31d50b5e19fa7352137e88bad9d61445cd146dcf
Eirik Nilsen <eirik.nilsen@deneb.no>
2011-11-15 15:34:15 +0100

    taber under character

--- warhammerApp/src/blargh/warhammer/android/CharacterFragment.java
+++ warhammerApp/src/blargh/warhammer/android/CharacterFragment.java
@@ -5,15 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 public class CharacterFragment extends Fragment {
 
 	public CharacterFragment(){
-		
 	}
+
+	
 	
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.character, container, false);
+		View view = inflater.inflate(R.layout.character, container, false);
+
+		TabHost tabhost = (TabHost) view.findViewById(android.R.id.tabhost);
+		tabhost.setup();
+		tabhost.addTab(tabhost.newTabSpec("stats").setIndicator("Stats").setContent(R.id.statsLayout));
+		tabhost.addTab(tabhost.newTabSpec("skills").setIndicator("Skills").setContent(R.id.skilllayout));
+		tabhost.addTab(tabhost.newTabSpec("talents").setIndicator("Talents").setContent(R.id.talentlayout));
+		tabhost.addTab(tabhost.newTabSpec("weapon").setIndicator("Weapon").setContent(R.id.weaponlayout));
+		tabhost.addTab(tabhost.newTabSpec("armor").setIndicator("Armor").setContent(R.id.armorlayout));
+		tabhost.addTab(tabhost.newTabSpec("gear").setIndicator("Gear").setContent(R.id.gearlayout));
+		tabhost.setCurrentTabByTag("stats");
+		
+		return view;
 	}
 }
